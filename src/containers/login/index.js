1ca20dcea62053257dfd01bc47c72a7927135213
ranggakusuma <ranggavskusuma@gmail.com>
2019-07-11 00:18:37 +0700

    add login and register form

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,22 +1,52 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import {
+  StyleSheet,
   Text,
-  Button
+  View,
+  TouchableOpacity,
 } from 'react-native';
+import LoginForm from '../../components/LoginForm';
 
 
 const App = (props) => {
   const { navigation: { navigate } } = props;
   return (
-    <Fragment>
-      <Text>Ini home</Text>
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigate('Register', { name: 'Jane' })}
-      />
-    </Fragment>
+    <View style={styles.container}>
+      <LoginForm type="Login" />
+      <View style={styles.signupTextCont}>
+        <Text style={styles.signupText}>Belum registrasi akun?</Text>
+        <TouchableOpacity onPress={() => navigate('Register')}>
+          <Text style={styles.signupButton}> Daftar</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#455a64',
+    // backgroundColor: 'red',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  signupTextCont: {
+    flexGrow: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    paddingVertical: 16,
+    flexDirection: 'row',
+  },
+  signupText: {
+    color: 'rgba(255,255,255,0.6)',
+    fontSize: 16,
+  },
+  signupButton: {
+    color: '#ffffff',
+    fontSize: 16,
+    fontWeight: '500',
+  }
+});
 
 export default App;
